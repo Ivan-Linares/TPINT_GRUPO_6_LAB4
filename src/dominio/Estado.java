1eea4f46cc2ebe41d91c03850871a5eac8d9fd19
Ivan-Linares <83107848+Ivan-Linares@users.noreply.github.com>
2023-06-22 01:15:52 -0300

    add turnos and estado class
    add turnos and estado class

---
+++ src/dominio/Estado.java
@@ -0,0 +1,27 @@
+package dominio;
+
+public class Estado {
+	
+	private int idEstado;
+	private String descripcion;
+	
+	public int getIdEstado() {
+		return idEstado;
+	}
+	public void setIdEstado(int idEstado) {
+		this.idEstado = idEstado;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public Estado(int idEstado, String descripcion) {
+		super();
+		
+		this.idEstado = idEstado;
+		this.descripcion = descripcion;
+	}
+}
---
+++ src/dominio/Turnos.java
@@ -0,0 +1,71 @@
+package dominio;
+
+import java.time.LocalDateTime;
+
+public class Turnos {
+	
+	private int IdTurno;
+	private LocalDateTime fechaHora;
+	private Especialidad especialidad;
+	private Paciente paciente;
+	private Estado estado;
+	private String observacion;
+	private boolean activo;
+	
+	
+	public Turnos(int idTurno, LocalDateTime fechaHora, Especialidad especialidad, Paciente paciente, Estado estado,
+			String observacion, boolean activo) {
+		super();
+		IdTurno = idTurno;
+		this.fechaHora = fechaHora;
+		this.especialidad = especialidad;
+		this.paciente = paciente;
+		this.estado = estado;
+		this.observacion = observacion;
+		this.activo = activo;
+	}
+	
+	public int getIdTurno() {
+		return IdTurno;
+	}
+	public void setIdTurno(int idTurno) {
+		IdTurno = idTurno;
+	}
+	public LocalDateTime getFechaHora() {
+		return fechaHora;
+	}
+	public void setFechaHora(LocalDateTime fechaHora) {
+		this.fechaHora = fechaHora;
+	}
+	public Especialidad getEspecialidad() {
+		return especialidad;
+	}
+	public void setEspecialidad(Especialidad especialidad) {
+		this.especialidad = especialidad;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	public Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	public String getObservacion() {
+		return observacion;
+	}
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+	public boolean isActivo() {
+		return activo;
+	}
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
+}
